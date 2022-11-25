e0ea56fab174d76bbcec537cad07108be86454fe
RehanMansoori <rehan@briskminds.com>
2022-11-25 12:40:12 +0530

    updates changes

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -80,12 +80,11 @@ define([
         var email = $('#email').val();
         console.log(email);
         if(email==''){ 
-            preventDefault();
             alert('email field cant be empty');
             document.getElementById('email').style.border= "2px solid red";
             document.getElementsByClassName('loader')[1].style.display = 'none';
             document.getElementsByClassName('loader-overlay')[0].style.display ='none';
-           
+            preventDefault();
             //spinner.remove();
            // return false;
         }
