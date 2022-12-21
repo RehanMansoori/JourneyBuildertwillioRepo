5a38be3858b9d9edf2fc4544f686f19b9e407f81
RehanMansoori <rehan@briskminds.com>
2022-12-21 17:13:26 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -129,23 +129,15 @@ exports.execute = function (req, res) {
         if (error) throw new Error(error);
         var body1 = JSON.parse(response1.body);
         console.log('body is', body1);
-        //console.log('email is = ',body1.email);
-        //res.send({"status" : "true"});
-        //res.send({"status" : "true"});
         console.log('status is = ',body1.isEmailActive__c);
         ischeck  = 'true'; //body1.isEmailActive__c;
         console.log('ischeck is = ', ischeck);
         if(ischeck == 'true'){
           console.log('-----------true0--------------');
-          //res.send({"status" : "true"});
+          res.send({"status" : "true"});
         }
       });
   });
-
-  if(ischeck == 'true'){
-    console.log('-----------true1--------------');
-    res.send({"status" : "true"});
-  }
 };
 
 
